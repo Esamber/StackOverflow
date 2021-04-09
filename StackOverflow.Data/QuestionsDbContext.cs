3db1070fafcca86b1a9fdd47c215d0bb4a783a4e
Elka Samber <ersamber@gmail.com>
2021-04-08 21:30:18 -0400

    Add project files.

---
+++ StackOverflow.Data/QuestionsDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace StackOverflow.Data
+{
+    public class QuestionsDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public QuestionsDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Likes> Likes { get; set; }
+        public DbSet<QuestionsTags> QuestionsTags { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<QuestionsTags>()
+                .HasKey(qt => new { qt.QuestionId, qt.TagId });
+
+            modelBuilder.Entity<QuestionsTags>()
+                .HasOne(qt => qt.Question)
+                .WithMany(q => q.QuestionsTags)
+                .HasForeignKey(qt => qt.QuestionId);
+
+            modelBuilder.Entity<QuestionsTags>()
+                .HasOne(qt => qt.Tag)
+                .WithMany(t => t.QuestionsTags)
+                .HasForeignKey(qt => qt.TagId);
+
+
+            modelBuilder.Entity<Likes>()
+                .HasKey(qu => new { qu.QuestionId, qu.UserId });
+
+            modelBuilder.Entity<Likes>()
+                .HasOne(qu => qu.Question)
+                .WithMany(q => q.Likes)
+                .HasForeignKey(qu => qu.QuestionId);
+
+            modelBuilder.Entity<Likes>()
+                .HasOne(qu => qu.User)
+                .WithMany(u => u.Likes)
+                .HasForeignKey(qu => qu.UserId);
+        }
+    }
+}
---
+++ StackOverflow.Data/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOverflow.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public List<Likes> Likes { get; set; }
+    }
+    public class Question
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public List<QuestionsTags> QuestionsTags { get; set; }
+        public List<Likes> Likes { get; set; }
+    }
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+    public class Likes
+    {
+        public int QuestionId { get; set; }
+        public int UserId { get; set; }
+        public Question Question { get; set; }
+        public User User { get; set; }
+    }
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+        public List<QuestionsTags> QuestionsTags { get; set; }
+    }
+    public class QuestionsTags
+    {
+        public int QuestionId { get; set; }
+        public int TagId { get; set; }
+        public Question Question { get; set; }
+        public Tag Tag { get; set; }
+    }
+
+}
